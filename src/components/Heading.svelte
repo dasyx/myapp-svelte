bdb2cecd38429feeac933f01f61b1a1fb3371cb8
Levast David <dasyx@MacBook-Pro-de-Levast.local>
2023-04-17 12:19:42 +0200

    test on dollar sign assignement + export components

---
+++ src/components/Heading.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let value;
+  // aucun valeur assignée à la variable ici puisque l'on va pouvoir donner des informations lorsque le composant sera utilisé (grâce à la balise "slot") 
+</script>
+
+<div>
+  <h1>{value}</h1>
+  <slot />
+  <!--la balise slot permet d'insérer du contenu à l'intérieur du composant-->
+</div>
--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,12 +1,23 @@
 <script>
-	export let count = 0;
+  let count = 0;
+  $: double = count * 2;
+  // la notation "$" est utilisée pour définir une exporession réactive qui sera recalculée chaque fois que l'une des variables qu'elle utilise est mise à jour
 
-	function incrementCount() {
-		count += 1;
-	}
+  // cette notation peut également être utilisée pour déclencher du code côté client, qui sera exécuté chaque fois que la variable qu'elle utilise est mise à jour
+  $: {
+    //console.log(`Le compteur est maintenant à ${count}`);
+	console.log('Le compteur est maintenant à ' + count);
+    //alert(`Le compteur est maintenant à ${count}`);
+  }
+
+  function incrementCount() {
+    count += 1;
+  }
 </script>
 
 <button on:click={incrementCount}>
-	Clicked {count}
-	{count <= 1 ? 'time' : 'times'}
+  Clicked {count}
+  {count <= 1 ? "time" : "times"}
 </button>
+
+<p>Le double de {count} est {double}</p>
